bd1874fd3d0543e3d778ee75bdd93ae5bdb9826c
Bruno André da Silva <38144738+brunoas17@users.noreply.github.com>
2025-01-03 21:08:50 +0000

    finalizando endpoint status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -3,11 +3,27 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export default async function status(request, response) {
-  const result = await prisma.$queryRawUnsafe(`SELECT 1 + 1 as sum`);
+  const updatedAt = new Date().toISOString();
 
-  console.log(result);
+  const databaseServerVersion =
+    await prisma.$queryRawUnsafe(`SHOW server_version;`);
 
-  response
-    .status(200)
-    .json({ message: "Aprender no curso.dev me torna acima da média" });
+  const databaseMaxConnections = await prisma.$queryRawUnsafe(
+    `SHOW max_connections;`,
+  );
+
+  const databaseOpenedConnections = await prisma.$queryRawUnsafe(
+    `SELECT count(*)::int FROM pg_stat_activity WHERE datname = 'local_db';`,
+  );
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseServerVersion[0].server_version,
+        max_connections: parseInt(databaseMaxConnections[0].max_connections),
+        opened_connections: databaseOpenedConnections[0].count,
+      },
+    },
+  });
 }
