67e86d110d3d33a84d6ec47c89bb6816c5ae5425
Bruno André da Silva <38144738+brunoas17@users.noreply.github.com>
2025-02-13 00:57:37 +0000

    fix eslint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -3,7 +3,6 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export default async function status(request, response) {
-  let varNotUsed;
   const updatedAt = new Date().toISOString();
 
   const databaseServerVersion =
