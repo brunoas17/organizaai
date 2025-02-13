11a1f4113d6efd1698ebe455277bc7bd55be1ebb
Bruno André da Silva <38144738+brunoas17@users.noreply.github.com>
2025-02-13 00:47:43 +0000

    adds  script with  && adds  job in  Github Action

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -3,6 +3,7 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export default async function status(request, response) {
+  let varNotUsed;
   const updatedAt = new Date().toISOString();
 
   const databaseServerVersion =
